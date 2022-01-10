b53fd363aeeb04353d010f1c587f703e58cb1f2d
Krista LaFentres <lafentres@users.noreply.github.com>
2022-01-10 15:38:09 -0600

    fix some panics/nil references

--- internal/command/show.go
+++ internal/command/show.go
@@ -101,23 +101,26 @@ func (c *ShowCommand) Run(args []string) int {
 		}
 	}
 
-	opts, err := c.contextOpts()
-	if err != nil {
-		diags = diags.Append(err)
-		c.showDiagnostics(diags)
-		return 1
-	}
-	tfCtx, ctxDiags := terraform.NewContext(opts)
-	diags = diags.Append(ctxDiags)
-	if ctxDiags.HasErrors() {
-		c.showDiagnostics(diags)
-		return 1
-	}
-	schemas, schemaDiags := tfCtx.Schemas(config, stateFile.State)
-	diags = diags.Append(schemaDiags)
-	if schemaDiags.HasErrors() {
-		c.showDiagnostics(diags)
-		return 1
+	if config != nil || stateFile != nil {
+		opts, err := c.contextOpts()
+		if err != nil {
+			diags = diags.Append(err)
+			c.showDiagnostics(diags)
+			return 1
+		}
+		tfCtx, ctxDiags := terraform.NewContext(opts)
+		diags = diags.Append(ctxDiags)
+		if ctxDiags.HasErrors() {
+			c.showDiagnostics(diags)
+			return 1
+		}
+		var schemaDiags tfdiags.Diagnostics
+		schemas, schemaDiags = tfCtx.Schemas(config, stateFile.State)
+		diags = diags.Append(schemaDiags)
+		if schemaDiags.HasErrors() {
+			c.showDiagnostics(diags)
+			return 1
+		}
 	}
 
 	if plan != nil {
